7440cf4c47c9ee7ab55a06fe82a06171d2424b81
robin <robinhahn99@outlook.de>
2023-12-21 23:09:33 +0100

    Added Name Override in Overview

--- addColons.py
+++ addColons.py
@@ -1,3 +1,5 @@
+import json
+
 # Name der Datei, die die Mod-Liste enthält
 datei_pfad = "Overview_Names.txt"
 
@@ -8,7 +10,22 @@ with open(datei_pfad, 'r') as file:
 # Jeden Mod-Namen formatieren
 formatierte_mod_liste = ['"{0}",'.format(mod) for mod in mod_list]
 
-# Ergebnisse in die gleiche Datei schreiben
 with open(datei_pfad, 'w') as file:
     for mod in formatierte_mod_liste:
+        if not mod.startswith('"'):
+            # Wenn der Mod-Name nicht mit Anführungszeichen beginnt, formatiere ihn entsprechend
+            mod = '"{0}",'.format(mod.strip())
         file.write(mod + '\n')
+
+# JSON-Objekt erstellen
+mod_json = {
+    "name": "random",
+    "version_number": "1.0.0",
+    "website_url": "https://github.com/thunderstore-io",
+    "description": "Mods for playing Company on demand.",
+    "dependencies": mod_list
+}
+
+# JSON-Objekt als manifest.json speichern
+with open('manifest.json', 'w') as json_file:
+    json.dump(mod_json, json_file, indent=2)
