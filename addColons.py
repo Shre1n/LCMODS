ac5e82c00a2174fd23fdd48439b97c5774ee95e0
Pandhazar <veilchen345@gmail.com>
2023-12-22 13:56:18 +0100

    Manifest Modlist working

--- addColons.py
+++ addColons.py
@@ -7,17 +7,6 @@ datei_pfad = "Overview_Names.txt"
 with open(datei_pfad, 'r') as file:
     mod_list = [line.strip() for line in file]
 
-# Jeden Mod-Namen formatieren
-formatierte_mod_liste = ['"{0}",'.format(mod) for mod in mod_list]
-
-with open(datei_pfad, 'w') as file:
-    for mod in formatierte_mod_liste:
-        if not mod.startswith('"'):
-            # Wenn der Mod-Name nicht mit Anführungszeichen beginnt, formatiere ihn entsprechend
-            mod = '"{0}",'.format(mod.strip())
-        file.write(mod + '\n')
-
-# JSON-Objekt erstellen
 mod_json = {
     "name": "random",
     "version_number": "1.0.0",
