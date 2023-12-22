823ad69e7c72955e02d0d81891b42c4a512c7b5a
robin <robinhahn99@outlook.de>
2023-12-22 15:39:36 +0100

    Formatted Dependencies but Override Names doesnt work

--- addColons.py
+++ addColons.py
@@ -2,6 +2,7 @@ import json
 
 # Name der Datei, die die Mod-Liste enthält
 datei_pfad = "Overview_Names.txt"
+manifest_json = "manifest.json"
 
 # Liste von Mod-Namen aus der Datei einlesen
 with open(datei_pfad, 'r') as file:
@@ -11,23 +12,36 @@ with open(datei_pfad, 'r') as file:
 formatierte_mod_liste = ['"{0}",'.format(mod) if not mod.startswith('"') else mod for mod in mod_list]
 
 # Verbinde die formatierten Mod-Namen mit neuen Zeilenzeichen
-mod_names_as_text = "\n".join(formatierte_mod_liste)
+mod_names_as_text = ",\n".join(formatierte_mod_liste)
 
 with open(datei_pfad, 'w') as file:
     for mod in formatierte_mod_liste:
-        if not mod.startswith('"'):
-            # Wenn der Mod-Name nicht mit Anführungszeichen beginnt, formatiere ihn entsprechend
-            mod = '"{0}",'.format(mod.strip())
-        file.write(mod + '\n')
+        file.write(mod + "\n")
 
 mod_json = {
     "name": "random",
     "version_number": "1.0.0",
     "website_url": "https://github.com/Shre1n/LCMODS",
     "description": "Mods for playing Company on demand.",
-    "dependencies": mod_list
+    "dependencies": formatierte_mod_liste
 }
 
-# JSON-Objekt als manifest.json speichern
-with open('manifest.json', 'w') as json_file:
-    json.dump(mod_json, json_file, indent=2)
+# Laden der JSON-Datei oder Erstellen eines leeren Manifests, wenn die Datei nicht vorhanden ist
+try:
+    with open(manifest_json, 'r') as file:
+        manifest_data = json.load(file)
+except FileNotFoundError:
+    manifest_data = {"dependencies": []}
+
+# Extrahieren der Dependencies-Liste aus dem JSON
+dependencies_list = manifest_data.get("dependencies", [])
+
+# Formatieren der Dependencies
+formatted_dependencies = [dep.strip('",') for dep in dependencies_list]
+
+# Aktualisieren des manifest_data mit den neu formatierten Dependencies
+manifest_data["dependencies"] = formatted_dependencies
+
+# Speichern des aktualisierten JSON in der Datei
+with open(manifest_json, 'w') as json_file:
+    json.dump(manifest_data, json_file, indent=2)
